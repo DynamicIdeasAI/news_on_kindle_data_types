d12293101c4035f5daef49b575bb3f646eefc42c
Capt. Michael <CaptMichaelDev@gmail.com>
2024-03-04 22:56:26 -0500

    feat: data type of rss

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './types/channel.type';
 export * from './types/category.type';
 export * from './types/notification.type';
 export * from './types/post.type';
+export * from './types/rss.type';
