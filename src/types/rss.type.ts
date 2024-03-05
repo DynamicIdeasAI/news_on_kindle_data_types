7bfbd4ec537165c492beb205fabfbe9b89a7259d
Capt. Michael <CaptMichaelDev@gmail.com>
2024-03-04 23:30:38 -0500

    feat: data type of rss entity

--- src/types/rss.type.ts
+++ src/types/rss.type.ts
@@ -1,14 +1,21 @@
 import type { BaseDataType } from '@dynamicideas/base-types';
 
-interface RssDataType extends BaseDataType {
-  title?: string;
+interface RssEntityDataType extends BaseDataType {
+  id: string;
+  title: string;
+  link: string;
   description?: string;
-  link?: string;
-  image?: {
-    url?: string;
-    title?: string;
-    link?: string;
-  };
+  published: Date;
 }
 
-export type { RssDataType };
+interface RssDataType extends BaseDataType {
+  title?: string;
+  link?: string;
+  description?: string;
+  generator?: string;
+  language?: string;
+  published?: Date;
+  entries?: RssEntityDataType[];
+}
+
+export type { RssEntityDataType, RssDataType };
