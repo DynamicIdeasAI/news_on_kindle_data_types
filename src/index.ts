dbaf7e29a45f190a3c827061e9e87055d83567a2
Capt. Michael <CaptMichaelDev@gmail.com>
2024-03-04 19:51:20 -0500

    feat: data type of post

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './types/base.type';
 export * from './types/channel.type';
 export * from './types/category.type';
 export * from './types/notification.type';
+export * from './types/post.type';
---
+++ src/types/post.type.ts
@@ -0,0 +1,14 @@
+import { BaseModelType } from '@dynamicideas/base-types';
+
+interface PostDataType extends BaseModelType {
+  channelId: string;
+  url: string;
+  originalHtmlContent: string;
+  title: string;
+  content: string;
+  summary?: string;
+  publishedAt?: Date;
+  authors?: string[];
+}
+
+export type { PostDataType };
