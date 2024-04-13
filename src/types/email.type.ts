3bbb0aa09521c4f13c241e8311217c2eedfd510b
Capt. Michael <CaptMichaelDev@gmail.com>
2024-04-13 11:15:49 -0400

    feat: cc in email data type

--- src/types/email.type.ts
+++ src/types/email.type.ts
@@ -3,6 +3,7 @@ import type { BaseModelType } from '@dynamicideas/base-types';
 interface EmailDataType extends BaseModelType {
   from: string;
   to: string[];
+  cc?: string[];
   subject: string;
   html: string;
   attachments: { filename: string; content: Buffer }[];
