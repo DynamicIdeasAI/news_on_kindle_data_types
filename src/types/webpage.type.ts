531da565a5bad61de9304b7bc89695c947d2e104
Capt. Michael <CaptMichaelDev@gmail.com>
2024-04-22 19:43:47 -0400

    perf: webpage data type

--- src/types/webpage.type.ts
+++ src/types/webpage.type.ts
@@ -1,9 +1,9 @@
 import type { BaseModelType } from '@dynamicideas/base-types';
 
 interface WebpageDataType extends BaseModelType {
-  url: string;
   userId?: string;
   username: string;
+  encodedUrl: string;
 }
 
 export type { WebpageDataType };
